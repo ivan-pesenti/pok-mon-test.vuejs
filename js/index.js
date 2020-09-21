aa099634b47a4ffd285cfec55c76cc4f3b7eaa42
Ivan Pesenti <ivan.pesenti@abenergie.it>
2020-09-21 17:35:18 +0200

    Commit after retrieving some initial data from API.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
 var app = new Vue({
-    el: '#app-container'
+    el: '#main-component',
+    data: {
+        pokemonList: null,
+        pokemonListEnriched: null,
+        count: 0,
+        error: ''
+    },
+    mounted: function() {
+        axios
+        .get('https://pokeapi.co/api/v2/pokemon/?limit=9&offset=')
+           .then(response => {            
+                this.pokemonList = response.data.results;               
+           })
+           .catch(function (error) {
+            vm.error = 'Could not reach the PokeAPI.co!';
+           }); 
+    }    
 });
