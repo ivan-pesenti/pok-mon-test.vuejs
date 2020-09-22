3e4e1ed113c05a8e10e9d0cbf133ce1feb328f63
Ivan Pesenti <ivan.pesenti@abenergie.it>
2020-09-22 14:55:58 +0200

    Before switching the call to poke api

--- docs/Todo.md
+++ docs/Todo.md
@@ -1,5 +1,5 @@
+# implement filter on series of pokémon
 # setup all of the sections in html
 # select fonts from Google fonts.
 # select color palette
 # replace div with components of Vue
-# order by id pokemon displayed
--- js/index.js
+++ js/index.js
@@ -3,13 +3,33 @@ function Pokemon() {
     this.name = '';
     this.img = '';
     this.types = [];
+    this.backgroundStyles = {};
+}
+
+function formatStyles(typesArr) {
+    if (typesArr.length == 1) {
+        let clrOne = getComputedStyle(document.documentElement)
+        .getPropertyValue('--' + typesArr[0] + '-clr');
+        return {
+            background: 'linear-gradient(135deg, ' + clrOne + ' 100%, #fff 0%)'
+        };
+    }
+    else {
+        let clrOne = getComputedStyle(document.documentElement)
+        .getPropertyValue('--' + typesArr[0] + '-clr');
+        let clrTwo = getComputedStyle(document.documentElement)
+        .getPropertyValue('--' + typesArr[1] + '-clr');
+        return {
+            background: 'linear-gradient(135deg, ' + clrOne + ' 55%, ' + clrTwo + ' 45%)'
+        }   
+    }
 }
 
 var app = new Vue({
     el: '#main-component',
     data: {
-        pokemonList: null,
-        pokemonListEnriched: [],
+        pokemonListRaw: null,
+        pokemonList: [],
         count: 0,
         error: ''
     },
@@ -17,13 +37,12 @@ var app = new Vue({
         axios
         .get('https://pokeapi.co/api/v2/pokemon/?limit=9&offset=')
            .then(response => {            
-                this.pokemonList = response.data.results;   
+                this.pokemonListRaw = response.data.results;   
 
-                this.pokemonList.forEach(element => {
+                this.pokemonListRaw.forEach(element => {
                     axios
                         .get(element.url)
                         .then(innerResponse => {
-                            // this.pokemonListEnriched.push(innerResponse.data.sprites)
                             
                             var pokemonToAdd = new Pokemon();
                             pokemonToAdd.id = innerResponse.data.id;
@@ -32,8 +51,16 @@ var app = new Vue({
                             pokemonToAdd.types = innerResponse.data.types.map(obj => {
                                 return obj.type.name;
                             });
+                            pokemonToAdd.types = pokemonToAdd.types.sort((a, b) => {
+                                return (a > b) ? 1 : -1;
+                            });
+                            pokemonToAdd.backgroundStyles = formatStyles(pokemonToAdd.types);
 
-                            this.pokemonListEnriched.push(pokemonToAdd);
+                            this.pokemonList.push(pokemonToAdd);
+
+                            this.pokemonList.sort((a, b) => {
+                                return (a.id > b.id) ? 1 : -1;
+                            });
                         })
                         .catch(error => console.error(error));
                 });
