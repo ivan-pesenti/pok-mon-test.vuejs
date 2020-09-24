c5430e5aba4029d8d9011a9bc8e9fe287ab5562d
Ivan Pesenti <ivan.pesenti@abenergie.it>
2020-09-24 18:03:45 +0200

    Commit after switch to in-memory variable PokemonList
    Now I've to build upon this variable all of the
    displaying logic.

--- js/index.js
+++ js/index.js
@@ -95,7 +95,7 @@ async function getPokemonFromApiByType(pokemonListByGeneration, offset, take, ty
                 })
                 .catch(error => console.error(error)); 
     }
-    
+
     return pokemonList.slice(offset, offset + take);
 }
 
@@ -114,14 +114,88 @@ async function getPokemonById(id){
         return pokemon;
 }
 
+async function loadPokemonFromApi(generarion = 1) {
+    var pokemonListRaw = [];
+    var pokemonList = [];
+
+    await axios
+        .get('https://pokeapi.co/api/v2/generation/' + generarion + '/')
+        .then(response => {
+            pokemonListRaw = response.data.pokemon_species;
+            
+            // add id property to the pokémon for sorting purpose
+            pokemonListRaw.forEach(element => {
+                element.url = element.url.slice(0, -1);
+                element.id = parseInt(element.url.substring(element.url.lastIndexOf("/") + 1));
+            });
+            
+            // order pokemon based on id
+            pokemonListRaw.sort((a, b) => {
+                return (a.id > b.id) ? 1 : -1;
+            });
+
+        })
+        .catch(error => console.error(error));
+
+        
+
+        for (let index = 0; index < pokemonListRaw.length; index++) {
+            const element = pokemonListRaw[index];
+            
+            await axios
+                    .get(element.url.replace('-species', ''))
+                    .then(innerResponse => {
+                        var pokemonToAdd = new Pokemon();
+                        pokemonToAdd.id = innerResponse.data.id;
+                        pokemonToAdd.name = innerResponse.data.name;
+                        pokemonToAdd.img = innerResponse.data.sprites.front_default;
+                        pokemonToAdd.types = innerResponse.data.types.map(obj => {
+                            return obj.type.name;
+                        });
+                        pokemonToAdd.types = pokemonToAdd.types.sort((a, b) => {
+                            return (a > b) ? 1 : -1;
+                        });
+    
+                        pokemonToAdd.backgroundStyles = formatStyles(pokemonToAdd.types);
+    
+                        pokemonList.push(pokemonToAdd);
+    
+                    })
+                    .catch(error => {console.error(error)});
+        }
+        
+        return pokemonList;
+}
+
+async function loadPokemonTypesFromApi(){
+    var types = [];
+    await axios 
+            .get('https://pokeapi.co/api/v2/type/')
+            .then(response => {
+                types = response.data.results.map(obj => {
+                    return obj.name;
+                });
+            })
+            .catch(error => console.error(error));
+
+    return types;
+}
+
+function pagingPokemonList(pokemonList, offset, limit) {
+    return pokemonList.slice(offset, offset + limit);
+}
+
 var app = new Vue({
     el: '#app-container',
     data: {
-        pokemonListRaw: null,
+        pokemonListRaw: [],
         pokemonList: [],
         selectedPokemon: null,
         count: 0,
         error: '',
+        displayObject: {
+            pokemonList: []
+        },
         pagingObject: {
             itemsPerPage: 10,
             offset: 0,
@@ -136,41 +210,32 @@ var app = new Vue({
         }
     },
     mounted: function() {
-        axios
-        .get('https://pokeapi.co/api/v2/generation/1/')
-           .then(response => {            
-                this.pokemonListRaw = response.data.pokemon_species;  
+        // in this section we're going to load all of the data from the PokeAPI.co
+        // the resources needed are Pokémon and types   
+        
+        loadPokemonFromApi(1)
+            .then(response => {               
+                this.pokemonList = response;
+                this.displayObject.pokemonList = pagingPokemonList(this.pokemonList, this.pagingObject.offset, this.pagingObject.itemsPerPage);
 
-                // add id property to the pokémon for sorting purpose
-                this.pokemonListRaw.forEach(element => {
-                    element.url = element.url.slice(0, -1);
-                    element.id = parseInt(element.url.substring(element.url.lastIndexOf("/") + 1));
-                });
-
-                // initialize pagingObject with values about page
-                this.pagingObject.totalPages = Math.round(this.pokemonListRaw.length / this.pagingObject.itemsPerPage + 1);
-                
-                // place the call with default values in order to get some initial content
-                getPokemonFromApi(this.pokemonListRaw, this.pagingObject.offset, this.pagingObject.itemsPerPage)
-                    .then(result => {
-                        this.pokemonList = result;
-                    })
-                    .catch(error => console.error(error));  
-
-            })
-            .catch(function (error) {
-                vm.error = error + 'Could not reach the PokeAPI.co!';
-            });
-            
-
-        axios
-            .get('https://pokeapi.co/api/v2/type/')
-            .then(response => {
-                this.filterObject.types = response.data.results.map(obj => {
-                    return obj.name;
-                });
             })
             .catch(error => console.error(error));
+
+        loadPokemonTypesFromApi()
+            .then(response => {
+                this.filterObject.types = response;
+            })
+            .catch(error => console.error(error));
+
+
+        // axios
+        //     .get('https://pokeapi.co/api/v2/type/')
+        //     .then(response => {
+        //         this.filterObject.types = response.data.results.map(obj => {
+        //             return obj.name;
+        //         });
+        //     })
+        //     .catch(error => console.error(error));
            
     },
     methods: {
@@ -231,6 +296,19 @@ var app = new Vue({
             getPokemonFromApiByType(this.pokemonListRaw, this.pagingObject.offset, this.pagingObject.itemsPerPage, searchedType)
                 .then(result => {
                     this.pokemonList = result;
+                    this.pagingObject.offset = 0;
+                    this.pagingObject.isPreviousBtnDisabled = true;
+                    if (result.length <= this.pagingObject.itemsPerPage) {
+                        this.pagingObject.totalPages = 1
+                        this.pagingObject.isNextBtnDisabled = true;
+                    } else {
+                        this.pagingObject.totalPages = Math.round(result.length / this.pagingObject.itemsPerPage + 1);
+                        // check to see if next button could be clicked
+                        if ((result.length - this.pagingObject.offset) < this.pagingObject.itemsPerPage) {
+                            this.pagingObject.isNextBtnDisabled = true;
+                        }
+                    }
+                    this.currentPage = 1;
                 })
                 .catch(error => console.error(error));
         }
