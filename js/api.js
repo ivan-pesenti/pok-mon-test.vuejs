8d69b6165cbb7acb37f44cf8928a6689d94baf3d
Ivan Pesenti <ivan.pesenti@abenergie.it>
2020-10-05 16:50:03 +0200

    Now I use the Promise.all()
    The api requests run in parallel now.
    Now there is another ninja revision.

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-import  { Pokemon }  from "./Pokemon.js";
+import { Pokemon } from "./Pokemon.js";
 import { formatStyles } from "./utils.js";
 
 async function loadPokemonFromApi(generarion = 1) {
@@ -9,13 +9,13 @@ async function loadPokemonFromApi(generarion = 1) {
         .get('https://pokeapi.co/api/v2/generation/' + generarion + '/')
         .then(response => {
             pokemonListRaw = response.data.pokemon_species;
-            
+
             // add id property to the pokémon for sorting purpose
             pokemonListRaw.forEach(element => {
                 element.url = element.url.slice(0, -1);
                 element.id = parseInt(element.url.substring(element.url.lastIndexOf("/") + 1));
             });
-            
+
             // order pokemon based on id
             pokemonListRaw.sort((a, b) => {
                 return (a.id > b.id) ? 1 : -1;
@@ -24,48 +24,55 @@ async function loadPokemonFromApi(generarion = 1) {
         })
         .catch(error => console.error(error));
 
-        
+    for (let i = 0; i < pokemonListRaw.length; i += 50) {
+        const requests = pokemonListRaw.slice(i, i + 50).map((request) => {
+            return axios
+                .get(request.url.replace('-species', ''))
+                .then(innerResponse => {
+                    var pokemonToAdd = new Pokemon(
+                        innerResponse.data.id,
+                        innerResponse.data.name,
+                        innerResponse.data.sprites.front_default,
+                        innerResponse.data.types.map(obj => {
+                            return obj.type.name;
+                        }),
+                        formatStyles(innerResponse.data.types)
+                    );
 
-        for (let index = 0; index < pokemonListRaw.length; index++) {
-            const element = pokemonListRaw[index];
-            
-            await axios
-                    .get(element.url.replace('-species', ''))
-                    .then(innerResponse => {
-                        var pokemonToAdd = new Pokemon(
-                            innerResponse.data.id,
-                            innerResponse.data.name,
-                            innerResponse.data.sprites.front_default,
-                            innerResponse.data.types.map(obj => {
-                                return obj.type.name;
-                            }),
-                            formatStyles(innerResponse.data.types)
-                        );
+                    // sort types of Pokémon in order to present them everytime in the same way
+                    pokemonToAdd.types = pokemonToAdd.types.sort((a, b) => {
+                        return (a > b) ? 1 : -1;
+                    });
 
-                        // sort types of Pokémon in order to present them everytime in the same way
-                        pokemonToAdd.types = pokemonToAdd.types.sort((a, b) => {
-                            return (a > b) ? 1 : -1;
-                        });    
-                        
-                        pokemonList.push(pokemonToAdd);
-    
-                    })
-                    .catch(error => {console.error(error)});
-        }
-        
-        return pokemonList;
+                    pokemonList.push(pokemonToAdd);
+
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        });
+
+        await Promise.all(requests)
+            .catch(e => console.error(e));
+    }
+
+    pokemonList = pokemonList.sort((a, b) => {
+        return (a.id > b.id) ? 1 : -1;
+    });
+
+    return pokemonList;
 }
 
-async function loadPokemonTypesFromApi(){
+async function loadPokemonTypesFromApi() {
     var types = [];
-    await axios 
-            .get('https://pokeapi.co/api/v2/type/')
-            .then(response => {
-                types = response.data.results.map(obj => {
-                    return obj.name;
-                });
-            })
-            .catch(error => console.error(error));
+    await axios
+        .get('https://pokeapi.co/api/v2/type/')
+        .then(response => {
+            types = response.data.results.map(obj => {
+                return obj.name;
+            });
+        })
+        .catch(error => console.error(error));
 
     return types;
 }
